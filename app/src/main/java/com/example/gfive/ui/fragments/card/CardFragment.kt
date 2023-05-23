59f479c4bacde9090cd4ec2a376adda18821da7c
milad heyrani <m.heyrani1368@gmail.com>
2023-05-23 11:00:24 -0700

    catch data on card fragment_card.xml

--- app/src/main/java/com/example/gfive/ui/fragments/card/CardFragment.kt
+++ app/src/main/java/com/example/gfive/ui/fragments/card/CardFragment.kt
@@ -1,21 +1,27 @@
 package com.example.gfive.ui.fragments.card
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.example.gfive.R
+import com.example.gfive.data.database.entities.DeckEntity
 
 class CardFragment : Fragment() {
 
 
-
+    private val args by navArgs<CardFragmentArgs>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        var deck= args.deck
+        Toast.makeText(this.context, deck.deckName,Toast.LENGTH_SHORT).show()
         return inflater.inflate(R.layout.fragment_card, container, false)
     }
 
